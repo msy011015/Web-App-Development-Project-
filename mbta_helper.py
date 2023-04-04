1a4d7a59c03f68eee96564dbe1941c3f94d04667
msy011015 <122825559+msy011015@users.noreply.github.com>
2023-04-04 17:53:35 -0400

    update

--- mbta_helper.py
+++ mbta_helper.py
@@ -12,8 +12,6 @@ MAPBOX_TOKEN = "pk.eyJ1IjoibXN5MDExMDE1IiwiYSI6ImNsZnpxZjFneTB3aGwzb3Bhb3RubGRvZ
 MAPBOX_BASE_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
 MBTA_BASE_URL = "https://api-v3.mbta.com/stops"
 
-# A little bit of scaffolding if you want to use it
-
     
 def get_lat_long(place_name: str) -> tuple[str, str]:
     """
@@ -34,17 +32,6 @@ def get_lat_long(place_name: str) -> tuple[str, str]:
     # print(type(lat_long))
     return lat_long
         
-def get_transportation_means() -> str:
-    """
-    Prompts the user to enter their preferred transportation means and returns it as a string.
-    """
-    while True:
-        transportation_means = input("Enter your preferred transportation means (Bus, Subway, or Commuter Rail): ")
-        if transportation_means.lower() not in ['bus', 'subway', 'commuter rail']:
-            print("Invalid transportation means. Please enter Bus, Subway, or Commuter Rail.")
-        else:
-            return transportation_means.lower()
-
 
 def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
     """
@@ -52,7 +39,6 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
 
     See https://api-v3.mbta.com/docs/swagger/index.html#/Stop/ApiWeb_StopController_index for URL formatting requirements for the 'GET /stops' API.
     """
-    
     MBTA_API_KEY = "fb950d9d0aaa490aac903e818b264994"
     url = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
     with urllib.request.urlopen(url) as f:
@@ -61,47 +47,30 @@ def get_nearest_station(latitude: str, longitude: str) -> tuple[str, bool]:
         # pprint (response_data)
         if len(response_data['data']) == 0:
             return None
-        station_name = response_data['data'][0]['attributes']['name']
-        wheelchair_accessible = response_data['data'][0]['attributes']['wheelchair_boarding'] == 1
-        return (station_name, wheelchair_accessible)
-
+        nearest_stations = []
+        for i in range(min(3, len(response_data['data']))):
+            station_name = response_data['data'][i]['attributes']['name']
+            wheelchair_accessible = response_data['data'][i]['attributes']['wheelchair_boarding'] == 1
+            nearest_stations.append((station_name, wheelchair_accessible))
+        return nearest_stations
+    
+# def prediction(stopid):
+#     MBTA_API_KEY = "fb950d9d0aaa490aac903e818b264994"
+#     url = f"https://api-v3.mbta.com/predictions?api_key={MBTA_API_KEY}&sort=departure_time&filter%5Bstop%5D={stopid}"
+#     with urllib.request.urlopen(url) as f:
+#         response_text = f.read().decode('utf-8')
+#         response_data = json.loads(response_text)
+        
 
 def find_stop_near(place_name: str) -> tuple[str, bool]:
     """
-    Given a place name or address after select the transportation means you want, return the nearest MBTA stop and whether it is wheelchair accessible.
-    
+    Given a place name or address, return the nearest MBTA stop and whether it is wheelchair accessible.
 
     This function might use all the functions above.
     """
-    latitude, longitude = get_lat_long(place_name)
-    transportation_type = get_transportation_means()
-
-    if not transportation_type:
-        print("Invalid transportation means.")
-
-    transportation_type_mapping = {
-        1 : "Subway",
-        2 : "Commuter Rail",
-        3 : "Bus"
-    }
-
-    if not transportation_type:
-        print("Invalid transportation means.")
-        return None
-
-    MBTA_API_KEY = "fb950d9d0aaa490aac903e818b264994"
-    url = f"https://api-v3.mbta.com/stops?api_key={MBTA_API_KEY}&sort=distance&filter%5Blatitude%5D={latitude}&filter%5Blongitude%5D={longitude}"
-    with urllib.request.urlopen(url) as f:
-        response_text = f.read().decode('utf-8')
-        response_data = json.loads(response_text)
-        stops = response_data['data']
-    filtered_stops = [stop for stop in stops if stop['attributes']['vehicle_type'] in transportation_type_mapping.keys() and transportation_type_mapping[stop['attributes']['vehicle_type']] == transportation_type]
-    if len(filtered_stops) == 0:
-        print(f"No {transportation_type} stops found near {place_name}.")
-        return None
-    station_name = filtered_stops[0]['attributes']['name']
-    wheelchair_accessible = filtered_stops[0]['attributes']['wheelchair_boarding'] == 1
-    return (station_name, wheelchair_accessible)
+    latitude, longtitude = get_lat_long(place_name)
+    res = get_nearest_station(latitude, longtitude)
+    return res
 
 def main():
     """
